d4b74f02a2d78eb3aa878d672bfb4325987ee010
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-03-09 21:55:44 +0200

    App rename and clean-up

--- app/src/main/java/com/bogdanorzea/buggycaps/PresentationApp.java
+++ app/src/main/java/com/bogdanorzea/buggycaps/PresentationApp.java
@@ -1,16 +1,10 @@
-package com.bogdanorzea.beadbugbeanie;
+package com.bogdanorzea.buggycaps;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class PresentationApp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +12,5 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 }
--- app/src/test/java/com/bogdanorzea/buggycaps/ExampleUnitTest.java
+++ app/src/test/java/com/bogdanorzea/buggycaps/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.bogdanorzea.beadbugbeanie;
+package com.bogdanorzea.buggycaps;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
